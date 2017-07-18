cd472b8a4fdb8a113236bf20bab676f4f7a6dd43
threetown <threetowns@163.com>
2017-07-18 18:25:04 +0800

    [Vue] Add: common.less and base.js

--- src/assets/js/Lib.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-
-import M from './common';
-
-export default{
-  M
-}
---
+++ src/assets/js/base.js
@@ -0,0 +1,9 @@
+
+
+import C from './common'
+
+require('assets/less/common.less')
+
+export default{
+  C
+}
