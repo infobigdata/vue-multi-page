b7d1cd852fe2fb419ff9fd484bbf30201f572d1c
threetown <threetowns@163.com>
2017-08-07 17:28:46 +0800

    Add: Api fills and template javascript.

--- src/api/data/template.js
+++ src/api/data/template.js
@@ -5,8 +5,8 @@ export const template = {
         "status":"success",
         "message":null,
         "data":{
-            "chartsids":[],
-            "html":"<ul id=\"gridter_ul\">\n</ul>",
+            "chartsids":["5199"],
+            "html": "<ul id=\"gridter_ul\">\n\t\t\t<li id=\"5199\" date=\"2017-07-28\" time=\"09:11:52\" data-row=\"1\" data-col=\"1\" data-sizex=\"8\" data-sizey=\"9\" class=\"gs-w\">\r\n\t\t\t\t<i class=\"handleBox\"></i>\r\n\t\t\t\t<div class=\"contentBox\">\r\n\t\t\t\t\t<!-- <span class=\"close\">删除节点</span> -->\n\t\t\t\t\t<div class=\"overflow\">\n\t\t\t\t\t\t<h3 class=\"ss-headline\">投诉分析</h3>\r\n\t\t\t\t\t\t<p class=\"xgicon\">\n\t\t\t\t\t\t\t\t<a class=\"xg_suaxing\" onclick=\"dashboard.flushChart('5199')\" title=\"刷新\"></a>\r\n\t\t\t\t\t\t\t\t<a class=\"xg_biaji\" href=\"/chart/edit/825/5199\" title=\"编辑\"></a>\n\t\t\t\t\t\t\t\t<a class=\"xg_quanping\" onclick=\"chartFullScreen.scan('5199')\" title=\"全屏\"></a>\n\t\t\t\t\t\t\t\t<a class=\"xg_shujugx\" ></a>\r\n\t\t\t\t\t\t\t\t<a class=\"xg_gengduo\" title=\"更多\"></a>\r\n\t\t\t\t\t\t</p>\n\t\t\t\t\t</div>\r\n\t\t\t\t\t<div id=\"chart_5199\" style=\"display:block;width: 100%; height: calc(100% - 20px);\" />\r\n\t\t\t\t</div>\r\n\t\t\t\t<span class=\"gs-resize-handle gs-resize-handle-both\"></span>\n\t\t\t\t<input id=\"table_5199\" type=\"hidden\" value=\"986\"/>\r\n\t\t\t</li>\r\n</ul>",
             "semantic_table_id":null,
             "baseinfohtml":"<h3 class=\"tybp_h2\">投诉分析</h3>\r\n<div class=\"nav_2_div float_clear\">\r\n\t<a class=\"nav_2_div_a fxybp\" onclick=\"dashboard.sharedDashboard()\"><i></i><span>分享</span></a>\r\n\t<a class=\"nav_2_div_a dcybp\" onclick=\"dashboard.exportDashbaord('239')\"><i></i><span>导出仪表盘</span></a>\r\n\t<a class=\"nav_2_div_a tjtb\" onclick=\"chartAdd.init()\"><i></i><span>添加图表</span></a>\r\n\t\t<a class=\"nav_2_div_a thmb\" href=\"/dashboard/changTableData?tableid=874\"><i></i><span>替换模板数据</span></a>\r\n</div>\r\n"
         },
