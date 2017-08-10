0c6309bb4a9fbd1ce410556fd263b7261bc26151
threetown <threetowns@163.com>
2017-08-10 15:48:46 +0800

    Add: dashboard changeTableData tpl

---
+++ src/views/dashboard/changeTableData/changeTableDataApp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>change</h1>
+
+    <error></error>
+  </div>
+</template>
+
+<script>
+import error from 'components/error';
+
+export default {
+    components: {
+        error
+    }
+
+}
+</script>
+
